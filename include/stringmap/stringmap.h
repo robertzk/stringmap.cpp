87c9ae818547c001144f85c172b96a6fd74ff99b
RobertZK <technoguyrob@gmail.com>
2015-12-26 12:59:36 -0600

    Initial stringmap definition

--- include/stringmap/stringmap.h
+++ include/stringmap/stringmap.h
@@ -5,10 +5,24 @@
 #ifndef STRINGMAP_INCLUDE_STRINGMAP_H
 #define STRINGMAP_INCLUDE_STRINGMAP_H
 
+#include <string>
 #include <vector>
 
 namespace stringmap {
+  // Modeled after http://www-igm.univ-mlv.fr/~dr/COURS_STL/vector.h
   class stringmap {
+  public:
+    typedef std::string string;
+
+    typedef std::allocator<string> stringmap_allocator;
+    typedef stringmap_allocator::pointer pointer;
+    typedef stringmap_allocator::pointer iterator;
+    typedef stringmap_allocator::const_pointer const_iterator;
+    typedef stringmap_allocator::reference reference;
+    typedef stringmap_allocator::const_reference const_reference;
+    typedef stringmap_allocator::size_type size_type;
+    typedef stringmap_allocator::difference_type difference_type;
+
 
   };
 }
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "stringmap/stringmap.h"
+
+int main(int argc, char** argv) {
+  std::cout << sizeof(stringmap::stringmap) << std::endl;
+  return 0;
+}
+
