ee295d8c91ebe67a024eb46c4787b094b84a4ab6
RobertZK <technoguyrob@gmail.com>
2015-12-26 12:32:21 -0600

    Add LICENSE + AUTHORS files

---
+++ include/stringmap/stringmap.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2016 Robert Krzyzanowski. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STRINGMAP_INCLUDE_STRINGMAP_H
+#define STRINGMAP_INCLUDE_STRINGMAP_H
+
+#include <vector>
+
+namespace stringmap {
+  class stringmap {
+
+  };
+}
+
+#endif // STRINGMAP_INCLUDE_STRINGMAP_H
+
